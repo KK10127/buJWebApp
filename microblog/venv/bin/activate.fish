0d4c7711bb4f08d716ee334e480735de0ba5a332
Jay Montoya <jayjoplin9898@gmail.com>
2019-05-26 20:21:15 -0700

    remade venv

--- microblog/venv/bin/activate.fish
+++ microblog/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/JayMontoya/microblog/venv"
+set -gx VIRTUAL_ENV "/Users/JayMontoya/buJWebApp/microblog/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
