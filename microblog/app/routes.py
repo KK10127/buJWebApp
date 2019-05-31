da97eac729289abda0245b76a39a8f709736db71
Jay Montoya <jayjoplin9898@gmail.com>
2019-05-31 16:28:54 -0700

    login form functional on front enda

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 @app.route('/')
@@ -31,7 +31,23 @@ def index():
     return render_template('index.html', title = 'Home',
         user = user, posts = posts)
 
-@app.route('/login')
+# This tells Flask that this view function accepts GET and POST requests,
+# overriding the default, which is to accept only GET requests.
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    # creating a form object
     form = LoginForm()
+
+    # when the browser sends the GET request to recieve the web page with the
+    # form, this method is going to return False, so that in that case the
+    # function skips this 'if' statement and goes directly to render the
+    # template in the last line of the function
+    if form.validate_on_submit():
+
+        # the flash() function is a useful way to show a message to the user.
+        # alot of applications use this technique to let the user know if some
+        # action has been successful or not.
+        flash('Login requested for user {}, remember_me = {}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title = 'Sign In', form = form)
