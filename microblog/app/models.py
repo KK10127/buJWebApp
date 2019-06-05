964815b49ca5a8cc9e968ddf2b7ce12042dfac7a
Jay Montoya <jayjoplin9898@gmail.com>
2019-06-04 19:42:22 -0700

    changes to db model including posts table

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 # this class inherits from db.Model, a base class
 class User(db.Model):
@@ -7,7 +8,26 @@ class User(db.Model):
     email = db.Column(db.String(120), index =True, unique = True)
     password_hash = db.Column(db.String(128))
 
+    #
+    posts = db.relationship('Post', backref ='author',lazy='dynamic')
+
     # this method tells python how to print objects of this class, which is
     # going to be useful for debugging.
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+# The Post class will represent blog posts written by users. The 'timestamp'
+# field is going to be idexed, which is useful if I want to retrieve posts in
+# chronological order.
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+
+    # These timestamps will be converted to the user's local time when they are
+    # displayed.
+    timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
