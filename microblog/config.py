2afde5a9ad0b7029c57c6f11c9ebaba6757344a0
Jay Montoya <jayjoplin9898@gmail.com>
2019-06-13 13:56:34 -0700

    cleaned error handling by designing a custom error html page for 404/505 errors. errors.py also created to handle DB rollback when necessary

--- microblog/config.py
+++ microblog/config.py
@@ -20,3 +20,10 @@ class Config(object):
     # not need, which is to signal the application every time a change is about
     # to be made in the database.git
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['jayjoplin9898@gmail.com']
