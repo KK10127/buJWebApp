e6c9351d08d3d762e0de5aa5c028fb96b06a023f
Jay Montoya <jayjoplin9898@gmail.com>
2019-06-18 17:34:53 -0700

    implemented the Home page functionality where users can now see posts from their followers - also updated html templates to better serve this feature

--- microblog/config.py
+++ microblog/config.py
@@ -27,3 +27,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['jayjoplin9898@gmail.com']
+
+    POSTS_PER_PAGE = 3
